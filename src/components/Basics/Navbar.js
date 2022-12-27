c35a0cea9cf6d4ab7b1cdaa32169904d347328c0
QudsiaAhmed <qudsiaahmed41@gmail.com>
2022-12-27 13:41:46 +0500

    rs web

---
+++ src/components/Basics/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const navbar = ({ filterItem, menuList }) => {
+  return (
+
+    <>
+    <h1>Resturant Menu</h1>
+      <nav className="navbar">
+        <div className="btn-group">
+          {menuList.map((curElem) => {
+            return (
+              <button
+                className="btn-group__item"
+                onClick={() => filterItem(curElem)}>
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default navbar;
---
+++ src/components/Basics/menuApi.js
@@ -0,0 +1,95 @@
+const Menu = [
+  {
+    id: 1,
+    image: "https://i.pinimg.com/originals/fd/b0/f3/fdb0f34bb9e6bac30c87afc3c9991e31.jpg",
+    name: "Egg",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 2,
+    image: "https://t3.ftcdn.net/jpg/04/29/00/48/360_F_429004893_0Y3UYCrn4mf2iYi4ASKAa10LBvSl32pt.jpg",
+    name: "Roll",
+    category: "evening",
+    price: "20₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 3,
+    image: "https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Healthy-porridge-bowl-b1f9205.jpg?quality=90&resize=768,574",
+    name: "Fresh Fruits",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 4,
+    image: "https://www.pakistanihealthyrecipes.com/wp-content/uploads/2021/05/pakistani-chicken-karahi-recipe-1024x683.jpg",
+    name: "Karahi",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 5,
+    image: "https://classic.exame.com/wp-content/uploads/2020/11/Muzzarela-1.jpg?quality=70&strip=info&w=1024",
+    name: "pizza",
+    category: "dinner",
+    price: "80₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 6,
+    image: "https://evakitchen.pk/wp-content/uploads/2018/08/seekh-kabab.jpg",
+    name: "Bar-B.Q",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 7,
+    image: "https://static5.depositphotos.com/1004137/465/i/600/depositphotos_4656385-stock-photo-indian-sweets.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "https://images.food52.com/McqpjxUiMekhfX6Rsq7wuuSoz0g=/2016x1344/filters:format(webp)/d815e816-4664-472e-990b-d880be41499f--chicken-biryani-recipe.jpg",
+    name: "Biryani",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 9,
+    image: "https://www.cookwithnabeela.com/wp-content/uploads/2021/08/Aloo_Samosa_Web_1.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 10,
+    image: "https://t4.ftcdn.net/jpg/01/58/36/13/360_F_158361356_PsgqLvsirkpM5n9hqCn48rexuB2UWsul.jpg",
+    name: "Nuggets",
+    category: "evening",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+];
+
+export default Menu;
